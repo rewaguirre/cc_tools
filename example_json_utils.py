710bee83f1a31a0639e162490cabe3917e1f2d2f
rewaguirre <36250955+rewaguirre@users.noreply.github.com>
2018-02-13 09:59:38 -0500

    notes from class
    copied Daves code as he gave us an example to follow

--- example_json_utils.py
+++ example_json_utils.py
@@ -6,6 +6,16 @@ import example_data
 def make_family_from_json(json_data):
     #Initialize a new family
     new_family = example_data.Family()
+    #trying to make this myself
+    new_family.parent1 = json_data["Parent1"]
+    new_family.parent2 = json_data["Parent2"]
+    for kid_data in json_data["Kids"]:
+        new_kid = example_data.Kid()
+        new_kid.name = kid_data["Name"]
+        new_kid.age = kid_data["Age"]
+
+        new_family.add_kid(new_kid)
+
     #Set the parents
     #Loop through the kids_data and make a new Kid for each entry in the kids_data
     #Note: this is how to loop through data in python
